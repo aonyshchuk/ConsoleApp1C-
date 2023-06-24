c8af4ca85dc1d94a50d95e8a6382151e846435f3
aonyshchuk <alinaonyshchuk7@gmail.com>
2023-06-24 16:46:09 +0300

    Add project files. Done 3.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+using static System.Console;
+
+/*
+ Користувач з клавіатури вводить шестизначне число. 
+Необхідно перевернути число і відобразити результат. 
+Наприклад, якщо введено 341256, результат 652143.
+ */
+
+namespace ConsoleApp1C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InputEncoding = Encoding.Unicode;
+            OutputEncoding = Encoding.Unicode;
+
+            Write("Введіть число: ");
+            string? str = Console.ReadLine();
+            WriteLine($"Результат: {str = new string(str.Reverse().ToArray())}");
+
+        }
+    }
+} 
